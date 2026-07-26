37269f79c39eac8c9a006df1405e26cfe65c5180
Aurélien <contact@web-wave.fr>
2026-07-26 09:21:21 +0200

    Exclusion du chargeur de firewall AIOS (fichier genere par l'environnement)
    Le mu-plugin aios-firewall-loader.php est reecrit par AIOS avec le chemin
    absolu de chaque environnement : il rejoint aios-bootstrap.php et .user.ini
    dans le .gitignore. Le fichier reste en place sur chaque serveur.

--- wp-content/mu-plugins/aios-firewall-loader.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Begin AIOWPSEC Firewall
-if (file_exists('/srv/data/web/vhosts/ditlproject.eu/htdocs/aios-bootstrap.php')) {
-	include_once('/srv/data/web/vhosts/ditlproject.eu/htdocs/aios-bootstrap.php');
-}
-// End AIOWPSEC Firewall
